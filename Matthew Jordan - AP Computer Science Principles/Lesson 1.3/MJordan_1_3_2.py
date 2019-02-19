c64f5aae8d37a5606d15debdf36c28247911545f
Matt <irishpacks@gmail.com>
2019-02-19 12:28:39 -0600

    Create directories + add files
    - Added lesson folder for 1.3 & added 1.3.2's activity in the aforementioned folder.

---
+++ Matthew Jordan - AP Computer Science Principles/Lesson 1.3/MJordan_1_3_2.py
@@ -0,0 +1,13 @@
+def add_tip(total, tip_percent):
+	'''Return the total amount of a meal including tip'''
+	return total + (tip_percent * total)
+
+def hyp(leg1, leg2):
+	return ((leg1 ** 2) + (leg2 ** 2)) ** 0.5
+
+def mean(a, b, c):
+	return (a + b + c) / 3
+
+def perimeter(base, height):
+	return (base * 2) + (height * 2)
+
